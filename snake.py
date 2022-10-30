6ef1fef16bdf10600b06a73e12b2eb797dfb4f09
Louis Lerouge <louis.lerouge@etu.minesparis.psl.eu>
2022-10-30 22:39:30 +0100

    serpent qui grandi

--- snake.py
+++ snake.py
@@ -6,13 +6,13 @@ import numpy as np
 # Changement
 
 pygame.init()
-screen = pygame.display.set_mode([1000, 1000])
+screen = pygame.display.set_mode([800, 800])
 clock = pygame.time.Clock()
-fruit=[random.randint(0,50),random.randint(0,50)]
+fruit=[random.randint(0,40),random.randint(0,40)]
 snake = [
     [10, 15],
     [11, 15],
-    [12, 15],]
+    [12, 15]]
 direction=[-1,0]
 while True:
     for event in pygame.event.get():
@@ -24,16 +24,16 @@ while True:
                 pygame.quit()
                 sys.exit()
             if event.key == pygame.K_UP:
-                direction=[0,-1] 
+                direction=[0,1] 
             if event.key == pygame.K_DOWN:
-                direction=[0,1]
+                direction=[0,-1]
             if event.key == pygame.K_RIGHT:
-                direction=[1,0]
+                direction=[-1,0]
             if event.key == pygame.K_LEFT:
-                direction=[-1,0]    
+                direction=[1,0]    
     color = [255, 255, 255]
     screen.fill(color)
-    x = np.arange(0,1000,20)  
+    x = np.arange(0,800,20)  
     width = 20
     height = 20
     red = 0
@@ -42,27 +42,26 @@ while True:
     color = [red, green, blue]
     for n in range(len(x)):
         if n%2==1:
-            y = np.arange(20,1000,40)
+            y = np.arange(20,800,40)
         else:
-            y = np.arange(0,1000,40)
+            y = np.arange(0,800,40)
         for val in y: 
             pygame.draw.rect(screen, color, [x[n],val,width,height])
     f=random.randint(0,50)
     if f==0:
-        fruit=[random.randint(0,50),random.randint(0,50)]
+        fruit=[random.randint(0,40),random.randint(0,40)]
     pygame.draw.rect(screen, [255,0,0],[fruit[0]*20, fruit[1]*20,20,20])
-    fin=snake[-1]
-    snake[2]=snake[1]
-    snake[1]=snake[0]
-    snake[0]=[snake[0][0]+direction[0],snake[0][1]+direction[1]]
+    snake.append([snake[-1][0]-direction[0],snake[-1][1]-direction[1]])
+    if snake[-1]!=fruit:
+        snake.pop(0)
+    elif snake[-1]==fruit:
+        fruit=[random.randint(0,40),random.randint(0,40)]
     for n in range(len(snake)):
         for m in range(n+1,len(snake)):
             if snake[n]==snake[m]:
                 pygame.quit()
                 sys.exit()
-    if snake[0]==fruit:
-        snake.append(fin)
     for pt in snake:
         pygame.draw.rect(screen, [0,255,0], [pt[0]*20,pt[1]*20,20,20])
     pygame.display.update()
-    clock.tick(5)
+    clock.tick(8)
